cf97ed2a9b39d8a8eef6878417337e5fc0eaa8fd
Nikita Krutoy <nikitakrutoy@gmail.com>
2016-03-22 11:07:11 +0300

    added download command

---
+++ crawler/download.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+from urllib.request import urlopen
+import os.path
+
+FILE_ROOT = 'articles/html/'
+
+
+def get_page_text(link):
+    try:
+        result = urlopen(link)
+    except:
+        print("Error occurred")
+    else:
+        if result.getcode() == 200:
+            page_text = result.read().decode('utf-8')
+            return page_text
+
+
+def download(urls):
+    if urls:
+        links = urls
+    else:
+        with open('links.txt', 'r') as lines:
+            links = lines.read().split('\n')
+    for link in links:
+        extension = '.txt'
+        file_name = link[link.rfind('/') + 1:]
+        print('Downloading ' + file_name + '...')
+        file_name += extension
+        file_path = FILE_ROOT + file_name
+        if not os.path.isfile(file_path):
+            page_text = get_page_text(link)
+            with open(file_path, 'w') as html:
+                html.write(page_text)
+    print('done')
